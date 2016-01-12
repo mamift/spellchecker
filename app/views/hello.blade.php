adb1c79d9f020690e578ce8ce38ad76a108e2098
Muhammad Miftah <muhammad.miftah@bpanz.com>
2016-01-12 12:02:02 +1000

    Minor changes; phpinfo.php and Command.php are placeholders.

---
+++ app/views/hello.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>BPA-SC</title>
+	
+	<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+</body>
+</html>
