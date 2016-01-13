1c89202b0a5c2f05ac4c31e000737f2fc5d9902f
Muhammad Miftah <muhammad.miftah@bpanz.com>
2016-01-14 08:42:29 +1000

    Added helper function get_ip_address() for a more reliable way to determine a client's IP address.

--- app/helpers.php
+++ app/helpers.php
@@ -155,6 +155,47 @@ if (!function_exists('get_namespaced_classes')) {
     }
 }
 
+if (!function_exists('get_ip_address')) {
+
+    /**
+     * Ensures an ip address is both a valid IP and does not fall within
+     * a private network range. Used by get_ip_address() above
+     */
+    function validate_ip($ip)
+    {
+        if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 | FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) === false) {
+            return false;
+        }
+        return true;
+    }
+
+}
+
+if (!function_exists('get_ip_address')) {
+
+    /**
+     * A slightly more reliable way to get a client's IP address
+     * 
+     * @return [string] [the client's IP address]
+     */
+    function get_ip_address() {
+        $ip_keys = array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR');
+        foreach ($ip_keys as $key) {
+            if (array_key_exists($key, $_SERVER) === true) {
+                foreach (explode(',', $_SERVER[$key]) as $ip) {
+                    // trim for safety measures
+                    $ip = trim($ip);
+                    // attempt to validate IP
+                    if (validate_ip($ip)) {
+                        return $ip;
+                    }
+                }
+            }
+        }
+        return isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : false;
+    }
+}
+
 if (!function_exists('is_upper_case')) {
     /**
      * If a word is uppercase or not...
