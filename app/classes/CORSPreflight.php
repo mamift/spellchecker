b05dc0f7958e64d2e5d302394013bf0a5d0250c0
Muhammad Miftah <muhammad.miftah@bpanz.com>
2016-02-26 12:32:18 +1000

    Got Cross Origin resource sharing working preflight channel requests working.

--- app/classes/CORSPreflight.php
+++ app/classes/CORSPreflight.php
@@ -6,42 +6,38 @@
 class CORSPreflight {
     
     /**
-     * Verifies that the CORS preflight is valid.
+     * Firefox is the one mainstream browser that seems to always want to preflight POST requests when it is CORS
+     * 
+     * @return [void or JSON]
      */
     public function filter()
     {
-        return;
-        
-        // $is_CORS_ok = $this->verifyCORSPreflight();
-        // if ($is_CORS_ok === true) {
-            // return;
-        // } else {
-            // return r403_json($is_CORS_ok);
-        // }
+        if (Request::method() !== 'OPTIONS') { // cors preflight requests are always with the OPTIONS verb
+            return r401_json(results_all('CORSPREFLIGHT2', false, CORSPREFLIGHT2));
+        }
+
+        if ($this->verifyCORSPreflight()) return r200_json(results_all(array('CORSPREFLIGHT_OK' => Request::method()), true, CORSPREFLIGHT_OK));
+        else return r401_json(results_all('CORSPREFLIGHT', false, CORSPREFLIGHT));
     }
 
+    /**
+     * Verifies that the CORS preflight request is valid.
+     * 
+     * @return [void or JSON] 
+     */
     public function verifyCORSPreflight()
     {
-        if (Request::method('OPTIONS')) {
-            $access_control_request_method = Request::header('Access-Control-Request-Method');
-            $access_control_request_headers = Request::header('Access-Control-Request-Headers');
+        $access_control_request_method = Request::header('Access-Control-Request-Method');
+        $access_control_request_headers = Request::header('Access-Control-Request-Headers');
 
-            $isset_acrm = isset($access_control_request_method);
-            $isset_acrh = isset($access_control_request_headers);
+        $isset_acrm = isset($access_control_request_method);
+        $isset_acrh = isset($access_control_request_headers);
 
-            $is_csrf_inside = strripos($access_control_request_headers, 'csrf') !== false;
-            $is_apikey_inside = strripos($access_control_request_headers, 'apikey') !== false;
+        $is_csrf_inside = isset($accrh) ? strripos($access_control_request_headers, 'csrf') !== false : false;
+        $is_apikey_inside = isset($acrm) ? strripos($access_control_request_headers, 'apikey') !== false : false;
 
-            if ($is_csrf_inside || $is_apikey_inside) return true;
-            else {
-                return results_all('CORSPREFLIGHT', false, CORSPREFLIGHT);
-                // return 'bad';
-            }
-        }
-    }
+        if ($is_csrf_inside && $is_apikey_inside) return;
 
-    public function isIE8XDRequest()
-    {
-        return;
+        return results_all('CORSPREFLIGHT', false, CORSPREFLIGHT);
     }
 }
